7f5261b8224d8cc654b92272aed966d2e4f175c0
Katyaaa341 <ilinykh_katyaa@mail.ru>
2020-06-28 22:43:56 +0700

    Write test for checkPath()

---
+++ test/test.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+
+#include "parse.h"
+#include "textFunctions.h"
+
+#define TEXT "test/test-Text.txt"
+#define RESULT "test/test-Result.txt"
+
+std::string text;
+std::vector<std::string> words;
+
+TEST(checkPathTest, correct)
+{
+    EXPECT_TRUE(checkPath(TEXT));
+    EXPECT_TRUE(checkPath(RESULT));
+}
